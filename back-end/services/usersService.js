bc2a22976763437eadfad97fdbc7f5d0c9d8bd46
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-09-22 17:34:48 -0300

    CR suggestions

--- back-end/services/usersService.js
+++ back-end/services/usersService.js
@@ -1,16 +1,18 @@
 const { generateJWT } = require('../middlewares');
 const { usersModel } = require('../models');
 
-const userLogin = async (email, password) => {
+const userLogin = async (email, pass) => {
   const user = await usersModel.getUserByEmail(email);
 
-  if (!user || user.password !== password) {
+  if (!user || user.password !== pass) {
     return {
       err: { code: 'invalid_entries', message: 'Wrong email or password' },
     };
   }
 
-  const { token } = generateJWT(user);
+  const { password, ...userData } = user;
+
+  const { token } = generateJWT(userData);
 
   return {
     name: user.name,
