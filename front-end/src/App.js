1ae75a6b9fa08fadda7fa8ad89e797922bd266e7
Rhian Castro <rhi.castro@gmail.com>
2020-09-24 15:50:05 -0300

    new pr

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
         <Route path="/">
           <HomePage />
         </Route>
---
+++ front-end/src/pages/LoginPage.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import useForm from '../hooks/useForm';
+
+export default function Login() {
+  const [isValid, setIsValid] = useState(true);
+  const { values, handleChange } = useForm({ email: '', password: '' });
+  const validade = (loginInfo) => {
+    const minPasswd = 6;
+    const password = loginInfo.password.length >= minPasswd;
+    const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    const email = expression.test(String(loginInfo.email).toLowerCase());
+    return password === email;
+  };
+  useEffect(() => {
+    setIsValid(validade(values));
+  }, [values]);
+  const handleSignInSubmit = async (e) => {
+    const { email, password } = values;
+    e.preventDefault();
+    const rawResponse = await fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const userInfo = await rawResponse.json();
+    if (userInfo.role === 'administrator') {
+      window.location.href = '/admin/orders';
+    }
+
+    if (userInfo.role === 'client') {
+      window.location.href = '/products';
+    }
+  };
+  const handleNoAccountSubmit = (e) => {
+    e.preventDefault();
+    window.location.href = '/register';
+  };
+  return (
+    <form>
+      <div className="mb-3">
+        <label className="form-label">Email:</label>
+        <input
+          type="email"
+          data-testid="email-input"
+          value={ values.email }
+          className="form-control"
+          id="email"
+          name="email"
+          onChange={ (event) => handleChange(event) }
+        />
+      </div>
+      <div className="mb-3">
+        <label value={ values.password } className="form-label" name="password">
+          Password:
+        </label>
+        <input
+          data-testid="password-input"
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          onChange={ (event) => handleChange(event) }
+        />
+      </div>
+      <div className="btn-group" role="group">
+        <div className="mb-3 mr-2">
+          <button
+            data-testid="signin-btn"
+            type="submit"
+            className="btn btn-primary"
+            disabled={ !isValid }
+            onClick={ (event) => handleSignInSubmit(event) }
+          >
+            ENTRAR
+          </button>
+        </div>
+        <div className="mb-3">
+          <button
+            onClick={ (event) => handleNoAccountSubmit(event) }
+            type="submit"
+            data-testid="no-account-btn"
+            className="btn btn-primary"
+          >
+            Ainda não tenho conta
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
