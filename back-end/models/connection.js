c69dac9a72cf097e4a476a9b8ab24f22413f3f4a
Rhian Castro <rhi.castro@gmail.com>
2020-09-24 19:19:43 -0300

    n a o

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -1,27 +1,25 @@
 require('dotenv').config();
 const mysqlx = require('@mysql/xdevapi');
 
-const config = {
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  host: process.env.HOSTNAME,
-  port: 33060,
-  socketPath: '/var/run/mysqld/mysqld.sock',
-};
-
-let schema;
-
-const connection = () => (schema
-  ? Promise.resolve(schema)
-  : mysqlx
-    .getSession(config)
-    .then(async (session) => {
-      schema = await session.getSchema('Trybeer');
+let schema = null;
+function connection() {
+  if (schema) return Promise.resolve(schema);
+  return mysqlx
+    .getSession({
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.HOSTNAME,
+      port: 33060,
+      schema: 'Trybeer',
+    })
+    .then((dbSchema) => {
+      schema = dbSchema;
       return schema;
     })
     .catch((err) => {
       console.error(err);
       process.exit(1);
-    }));
+    });
+};
 
 module.exports = connection;
