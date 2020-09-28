fb8334bff493e7a59e0d30e0f05de8d9fdf3d90a
Rodrigo Militao <militaorodrigo9@gmail.com>
2020-09-28 17:31:18 -0300

    lint -fix

--- front-end/src/pages/AdminProfile.js
+++ front-end/src/pages/AdminProfile.js
@@ -1,23 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 function AdminProfile() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [loggedName, setLoggedName] = useState('');
+  const [loggedEmail, setLoggedEmail] = useState('');
 
   useEffect(() => {
     const storageUser = localStorage.getItem('user');
-    if(!storageUser)  window.location.href = '/login';
+    if (!storageUser) window.location.href = '/login';
     const { name, email } = JSON.parse(storageUser) || {};
-    setName(name);
-    setEmail(email);
-
+    setLoggedName(name);
+    setLoggedEmail(email);
   }, []);
 
   return (
     <div>
       <h1>Perfil</h1>
-      <h5 data-testid="profile-name">Nome: { name }</h5>
-      <h5 data-testid="profile-email">Email: { email }</h5>
+      <h5 data-testid="profile-name">
+        Nome:
+        { loggedName }
+      </h5>
+      <h5 data-testid="profile-email">
+        Email:
+        { loggedEmail }
+      </h5>
     </div>
   );
 }
