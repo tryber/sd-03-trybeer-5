325ca570019940a9de0a34c9af4d560a5df82aff
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-10-01 14:40:44 -0300

    lint

--- front-end/src/services/ordersService.js
+++ front-end/src/services/ordersService.js
@@ -1,5 +1,4 @@
 const getAllClientOrders = async (id, token) => {
-  console.log(id, token);
   const response = await fetch(`http://localhost:3001/orders?id=${id}`, {
     headers: { authorization: token },
   });
