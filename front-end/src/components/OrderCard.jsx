05c15ec2728739eaaba685610ec898913e66ea77
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-10-05 15:08:21 -0300

    cria e implementa função de formatar data

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import formatDate from '../utils/formatDate';
 
 function OrderCard({ order, index }) {
   const { orderNumber, totalPrice, saleDate } = order;
+  //const formatedDate = new Date(saleDate);
   return (
-    <div className="card" data-testid={`${index}-order-card-container`}>
+    <div className="card product-card" data-testid={`${index}-order-card-container`}>
       <div className="card-body">
         <h5
           className="card-title"
           data-testid={`${index}-order-number`}
         >{`Pedido ${orderNumber}`}</h5>
         <h6 className="card-text" data-testid={`${index}-order-date`}>
-          {saleDate}
+          {formatDate(saleDate)}
         </h6>
         <h5 className="card-text" data-testid={`${index}-order-total-valueu`}>{totalPrice}</h5>
       </div>
