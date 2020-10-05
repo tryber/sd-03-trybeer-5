32cf8db6f4d3bda7729c2a717864613ebbb37000
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-10-05 15:15:08 -0300

    atualiza função de formatar data

--- front-end/src/utils/formatDate.js
+++ front-end/src/utils/formatDate.js
@@ -5,9 +5,9 @@ const formatDate = (date) => {
   const formatedDate = new Date(date);
   const day = formatedDate.getDate().toString();
   const month = (formatedDate.getMonth() + 1).toString();
-  const formatedDay = day.length === 1 ? '0' + day : day;
-  const formatedMonth = month.length === 1 ? '0' + month : month;
-  return formatedDay + '/' + formatedMonth;
+  const formatedDay = day.length === 1 ? `0${day}` : day;
+  const formatedMonth = month.length === 1 ? `0${month}` : month;
+  return `${formatedDay}/${formatedMonth}`;
 };
 
 module.exports = formatDate;
